5212bbf31bfd7ff07ea308572a29b249cae2df58
Simon Ellershaw <simje@live.co.uk>
2021-05-17 16:43:16 +0100

    Graham preprocessing using cv2

--- eyePACS.py
+++ eyePACS.py
@@ -2,10 +2,19 @@ import pandas as pd
 import os
 import torch
 import json
-from PIL import Image
+import numpy as np
 
 from torch.utils.data import Dataset
-from torchvision import transforms
+import torchvision
+import transforms
+import cv2
+from transforms import GrahamPreprocessingCV2
+import random
+
+# Test 
+import visualisation
+import matplotlib.pyplot as plt
+import time
 
 class EyePACS_Dataset(Dataset):
     def __init__(self, config_fname):
@@ -16,34 +25,44 @@ class EyePACS_Dataset(Dataset):
         self.img_dir = config["img_dir"]
         self.class_names = config["class_names"]
         # Setup differing transforms for training and testing
-        self.is_train_set = False
-        self.transform_train = transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
-        self.transform_test = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
-        
+        self.augment = False        
 
     def __len__(self):
+        # return 100
         return len(self.labels_df)
 
     def __getitem__(self, idx):
         # Extract sample's metadata
         metadata = self.labels_df.loc[idx]
         label = metadata.level
-        # Load and transform image
+        # Load and transform img
         img_path = os.path.join(self.img_dir, metadata.image + ".jpeg")
-        image = Image.open(img_path)
-        image = self.transform_train(image) if self.is_train_set else self.transform_test(image)
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if self.augment:
+            img = self.augmentation(img)
+        img = GrahamPreprocessingCV2(img)
+        img = cv2.resize(img, (224,224))
+        img = np.divide(img, 255.0, dtype=np.float32)
+        img = torch.from_numpy(img).permute(2, 0, 1)
         # Return sample
-        return image, label
+        return img, label
+    
+    def augmentation(self, img):
+        # Random flip
+        flip_code = random.randrange(-1,2) # -1 both, 0 x-axis, 1 y-axis, 2 none
+        if flip_code < 2:
+            img = cv2.flip(img, flip_code)
+        rows, cols, _ = img.shape
+        # Random translation
+        translation_x = random.uniform(-1, 1)*0.05*img.shape[0] 
+        translation_y = random.uniform(-1, 1)*0.05*img.shape[1]
+        translation_matrix = np.float32(([[1,0,translation_x],[0,1,translation_y]]))
+        img = cv2.warpAffine(img,translation_matrix,(cols,rows))
+        # Random rotation
+        angle = random.random()*45
+        rotation_matrix = cv2.getRotationMatrix2D((cols/2,rows/2), random.uniform(-1, 1)*45, 1)
+        return cv2.warpAffine(img,rotation_matrix,(cols,rows))
 
     def split_train_test_val_sets(self, prop_train, prop_val, prop_test):
         # Calc split in terms of num samples instead of proportions
@@ -51,6 +70,23 @@ class EyePACS_Dataset(Dataset):
         num_val = round(prop_val*len(self))
         num_test = len(self) - num_train - num_val
         # Split and set training set bool so correct transform occurs
-        dataset_train, dataset_val, dataset_test = torch.utils.data.random_split(self, [num_train, num_val, num_test])
+        dataset_train, dataset_val, dataset_test = torch.utils.data.random_split(self, [num_train, num_val, num_test], generator=torch.Generator().manual_seed(1))
         dataset_train.train_set = True
         return dataset_train, dataset_val, dataset_test
+                
+if __name__ == "__main__":
+    data = EyePACSCV2_Dataset("config.json")
+    data.augment = True
+    print(len(data))
+    # for i in range(10):
+    #     start_time = time.time()
+    #     x = data[i]
+    #     print(time.time()-start_time)
+    # data.is_train_set = False
+    # start_time = time.time()
+    # data = data[2]
+    # print(time.time()-start_time)
+    fig, ax = plt.subplots()
+    idx = 10
+    visualisation.imshow(data[idx][0], ax)
+    plt.show()
---
+++ transforms.py
@@ -0,0 +1,65 @@
+from PIL import ImageFilter, Image
+import numpy as np
+import math
+import cv2  
+
+def GrahamPreprocessing(image):
+    radius_scaled = 500       
+    radius_intial = estimate_radius(image)
+    radius_intial = radius_scaled if radius_intial == 0 else radius_intial
+    image = rescale_image(image, radius_intial, radius_scaled)
+    image = subtract_average_local_colour(image)
+    radius_boundary = round(radius_scaled*0.9)
+    image = threshold_boundary(image, radius_boundary)
+    image = crop_image(image, radius_boundary)
+    return image
+
+def estimate_radius(image):
+    # Take central row and sum accross channels
+    x = image[image.shape[0]//2].sum(1)
+    # ROI defined as non zero pixels
+    radius = (x>x.mean()/10).sum()/2
+    return radius
+
+def rescale_image(image, radius_intial, radius_scaled):
+    width, height, _ = image.shape
+    # Resize so radius of eye is = radius_scaled
+    s = radius_scaled/radius_intial
+    return cv2.resize(image, (0,0), fx=s, fy=s)
+
+def subtract_average_local_colour(image):
+    average_local_colour = cv2.GaussianBlur(image, (0,0), 10)
+    image = cv2.addWeighted(image, 4, average_local_colour, -4, 128)
+    return image
+
+def threshold_boundary(image, radius_boundary):
+    boundary = np.zeros(image.shape)
+    cv2.circle(boundary, (image.shape[1]//2, image.shape[0]//2), radius_boundary, (1,1,1), -1)
+    return cv2.multiply(image, boundary.astype("uint8"))
+
+def crop_image(image, radius_boundary):
+    # Crop to square the size of scale
+    centre_h = image.shape[0]//2
+    centre_w = image.shape[1]//2
+    if centre_h < radius_boundary:
+        image, centre_h = pad_height(image, 2*radius_boundary)
+    y_min = centre_h - radius_boundary
+    y_max = centre_h + radius_boundary
+    x_min = centre_w - radius_boundary
+    x_max = centre_w + radius_boundary
+    return image[y_min:y_max, x_min:x_max]
+
+def pad_height(image, desired_h):
+    h_padding = math.ceil((desired_h-image.shape[0])/2.0)
+    image = cv2.copyMakeBorder(image, h_padding, h_padding, 0, 0, cv2.BORDER_CONSTANT, value=[0,0,0])
+    return image, image.shape[0]//2
+
+if __name__ == "__main__":
+    start_time = time.time()
+    image = cv2.imread(r"C:\\Users\\rmhisje\Documents\\medical_ViT\\diabetic-retinopathy-detection\\train\\train\\16_left.jpeg")
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = GrahamPreprocessingCV2(image)
+    print(image.min())
+    print(time.time()-start_time)
+    axes[1].imshow(image)
+    plt.show()
