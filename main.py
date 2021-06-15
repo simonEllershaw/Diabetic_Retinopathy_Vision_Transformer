8ec012c3087618fbcf9e99e69aee834801ff1dc6
Simon ELLERSHAW <sellersh@beaker.cs.ucl.ac.uk>
2021-06-15 15:42:59 +0100

    Cluster friendly parameter selection

--- main.py
+++ main.py
@@ -27,25 +27,22 @@ if __name__ == "__main__":
     # Set up directory for experiment
     model_name = "resnet50"
     dataset_name = "_eyePACS_"
-    run_directory = os.path.join("runs", model_name+ dataset_name + time.strftime("%m_%d_%H_%M_%S"))
+    run_directory = os.path.join("runs", model_name + dataset_name + time.strftime("%m_%d_%H_%M_%S"))
     os.mkdir(run_directory)
 
     # Load datasets split into train, val and test
     dataset_names = ["train", "val", "test"]
-    data_directory = "diabetic-retinopathy-detection" 
-    # data_directory = sys.argv[1]
+    # data_directory = "diabetic-retinopathy-detection" 
+    data_directory = sys.argv[1]
     dataset_proportions = np.array([0.6, 0.2, 0.2])
     full_dataset = EyePACS_Dataset(data_directory, random_state=13)
     class_names = full_dataset.class_names
 
     datasets = full_dataset.create_train_val_test_datasets(dataset_proportions, dataset_names)
     datasets["train"].augment=True
-    
-    # with open(os.path.join(run_directory, "dataset_indexes.pkl"), "wb+") as index_file:
-    #     pickle.dump(dataset_indicies, index_file)
 
     # Setup dataloaders
-    batch_size= 64
+    batch_size= 100#64
     num_workers = 4
     dataset_sizes = {name: len(datasets[name]) for name in dataset_names}                  
     dataloaders = {name: torch.utils.data.DataLoader(datasets[name], batch_size=batch_size,
@@ -58,9 +55,6 @@ if __name__ == "__main__":
 
     train_freq = torch.tensor(data_train_labels.value_counts(sort=False).sort_index()).float()
     print(train_freq)
-    # weight = 1.0/train_freq
-    # weight = weight / torch.sum(weight) * len(class_names)
-
     weight = 1. / train_freq
     samples_weight = weight[data_train_labels.values]
     sampler = torch.utils.data.WeightedRandomSampler(samples_weight, len(samples_weight))
@@ -74,7 +68,8 @@ if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = timm.create_model(model_name, pretrained=True, num_classes=len(class_names), drop_rate=0.5).to(device)
     criterion = nn.CrossEntropyLoss()#weight=weight.to(device))
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    lr = sys.argv[2] #0.001
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
     warmup_steps = 10
     scheduler = LRSchedules.WarmupCosineSchedule(optimizer, num_epochs, warmup_steps)
     num_epochs_to_converge = 100
