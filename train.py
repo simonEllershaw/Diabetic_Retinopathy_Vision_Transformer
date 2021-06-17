a15fcabeba6435b5500b4de7643409edacc1d9d9
Simon Ellershaw <simje@live.co.uk>
2021-06-17 19:01:58 +0100

    F1 score used for model selection

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import visualisation
 #https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
 
 def train_model(model, dataloaders, optimizer, criterion, scheduler, num_epochs, device, dataset_sizes, nb_classes, writer, run_directory, warmup_steps, num_epochs_to_converge, grad_clip_norm=0):
-    best_loss = float('inf')
+    best_f1 = float('-inf')
     model_param_fname = os.path.join(run_directory, "model_params.pt")
     num_epochs_no_improvement = 0
 
@@ -39,15 +39,15 @@ def train_model(model, dataloaders, optimizer, criterion, scheduler, num_epochs,
                 running_loss += loss.item() * inputs.size(0)
                 confusion_matrix = metrics.update_conf_matrix(confusion_matrix, labels, preds)
             epoch_loss = running_loss / dataset_sizes[phase]  
-            epoch_kappa = metrics.calc_weighted_quadratic_kappa(confusion_matrix)           
             if phase == 'train':
                 # Update LR
                 writer.add_scalar(tag="general/lr", scalar_value=scheduler.get_last_lr()[0], global_step=epoch)
                 scheduler.step()
             elif phase == 'val':
                 # Check if model performance has improved if so save model
-                if epoch_loss < best_loss:
-                    best_loss = epoch_loss
+                epoch_f1_macro = metrics.calc_macro_f1_score(confusion_matrix)           
+                if epoch_f1_macro > best_f1:
+                    best_f1 = epoch_f1_macro
                     torch.save(model.state_dict(), model_param_fname)
                     num_epochs_no_improvement = 0
                     best_conf_matrix = confusion_matrix
@@ -64,7 +64,7 @@ def train_model(model, dataloaders, optimizer, criterion, scheduler, num_epochs,
     writer.add_figure(tag="Confusion Matrix/" + phase, figure=confusion_matrix_vis, global_step=epoch+1)
     model.load_state_dict(torch.load(model_param_fname))
     model.eval()
-    return model, best_loss
+    return model, best_f1  
 
 def write_epoch_statistics_to_tensorboard(writer, phase, epoch, epoch_loss, confusion_matrix, class_labels):
     # Calc statistics
