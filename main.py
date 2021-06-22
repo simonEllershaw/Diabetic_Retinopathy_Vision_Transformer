a7d078697a94de6bce7eef77209c97b96667ee3a
Simon Ellershaw <simje@live.co.uk>
2021-06-22 16:02:02 +0100

    Tidy up and implementation of training choices

--- main.py
+++ main.py
@@ -19,14 +19,16 @@ from train import train_model
 import visualisation
 from eyePACS import EyePACS_Dataset
 import sklearn.utils
+import evaluate
 
-#testing
+# Testing
 import numpy as np
 from torchvision import transforms, datasets
 import metrics
 import sklearn.metrics
 
 if __name__ == "__main__":
+    torch.cuda.empty_cache()
     # Set up directory for experiment
     print(sys.argv)
     data_directory = sys.argv[1] if len(sys.argv) > 1 else "diabetic-retinopathy-detection"
@@ -34,17 +36,15 @@ if __name__ == "__main__":
     lr = float(sys.argv[3]) if len(sys.argv) > 3 else 0.003
     
     dataset_name = "_eyePACS_"
-    run_directory = os.path.join("runs", model_name + dataset_name + time.strftime("%m_%d_%H_%M_%S"))
-    os.mkdir(run_directory)
+    model_directory = os.path.join("runs", model_name + dataset_name + time.strftime("%m_%d_%H_%M_%S"))
+    os.mkdir(model_directory)
+    print(model_directory)
 
     # Load datasets split into train, val and test
-    dataset_names = ["train", "val", "test"]
-    # data_directory = "diabetic-retinopathy-detection" 
-    
+    dataset_names = ["train", "val", "test"]    
     dataset_proportions = np.array([0.6, 0.2, 0.2])
-    full_dataset = EyePACS_Dataset(data_directory, random_state=13, max_length=1000)
+    full_dataset = EyePACS_Dataset(data_directory, random_state=13)#, max_length=1000)
     class_names = full_dataset.class_names
-
     datasets = full_dataset.create_train_val_test_datasets(dataset_proportions, dataset_names)
     datasets["train"].augment=True
 
@@ -59,45 +59,31 @@ if __name__ == "__main__":
 
     # Calc class inbalance and so loss function weights
     data_train_labels = np.array(datasets["train"].get_labels())
+    loss_weights = sklearn.utils.class_weight.compute_class_weight("balanced", classes=np.unique(data_train_labels), y=data_train_labels)
+    loss_weights = torch.tensor(loss_weights).float()
 
-    # train_freq = torch.tensor(data_train_labels.value_counts(sort=False).sort_index()).float()
-    weights = sklearn.utils.class_weight.compute_class_weight("balanced", classes=np.unique(data_train_labels), y=data_train_labels)
-    weights = torch.tensor(weights).float()
-    # # Calc class inbalance and so loss function weights
-    # data_train_labels = datasets["train"].get_labels()
-
-    # train_freq = torch.tensor(data_train_labels.value_counts(sort=False).sort_index()).float()
-    # print(train_freq)
-    # weight = 1. / train_freq
-    # samples_weight = weight[data_train_labels.values]
-    # sampler = torch.utils.data.WeightedRandomSampler(samples_weight, len(samples_weight))
-
-    # dataloaders["train"] = torch.utils.data.DataLoader(
-    #     datasets["train"], batch_size=batch_size, num_workers=num_workers, sampler=sampler)
-    # dataloaders["train"].shuffle = True
-
-    num_epochs = 100
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = timm.create_model(model_name, pretrained=True, num_classes=len(class_names)).to(device)
 
-    criterion = nn.CrossEntropyLoss(weight=weights.to(device))
-    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+    num_epochs = 100
     warmup_steps = 10
-    scheduler = LRSchedules.WarmupCosineSchedule(optimizer, num_epochs, warmup_steps)
-    num_epochs_to_converge = 25
+    num_epochs_to_converge = 10
     grad_clip_norm = 1
 
+    criterion = nn.CrossEntropyLoss(weight=loss_weights.to(device))
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+    scheduler = LRSchedules.WarmupCosineSchedule(optimizer, num_epochs, warmup_steps)
+    
     # Init tensorboard
-    writer = SummaryWriter(run_directory)
-
-    # # Add input images to tensorboard for sanity check
+    writer = SummaryWriter(model_directory)
+    # Add input images to tensorboard for sanity check
     fig = visualisation.sample_batch(dataloaders["train"], class_names)
     writer.add_figure('Input/train', fig)
     fig = visualisation.sample_batch(dataloaders["val"], class_names)
     writer.add_figure('Input/val', fig)
 
     # # Main training loop
-    model, best_acc = train_model(model, dataloaders, optimizer, criterion, scheduler, num_epochs, device, dataset_sizes, len(class_names), writer, run_directory, warmup_steps, num_epochs_to_converge, grad_clip_norm)
+    model, best_acc = train_model(model, dataloaders, optimizer, criterion, scheduler, num_epochs, device, dataset_sizes, len(class_names), writer, model_directory, warmup_steps, num_epochs_to_converge, grad_clip_norm)
 
     # Add sample inference outputs to tensorboard
     fig = visualisation.sample_batch(dataloaders["train"], class_names, model, device)
@@ -107,3 +93,4 @@ if __name__ == "__main__":
 
     writer.close()
 
+    evaluate.evaluate_model(model, device, dataloaders["val"], datasets["val"].get_labels(), batch_size, model_directory)
