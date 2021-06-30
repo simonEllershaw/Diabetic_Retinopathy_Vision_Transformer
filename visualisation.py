bc4762d96e6621bab69bd26de502133d2bf7ccb7
Simon Ellershaw <simje@live.co.uk>
2021-06-30 11:05:52 +0100

    Remove normalisation

--- visualisation.py
+++ visualisation.py
@@ -10,10 +10,6 @@ import itertools
 def imshow(inp, ax, title=None):
     """Imshow for Tensor."""
     inp = inp.numpy().transpose((1, 2, 0))
-    # mean = np.array([0.485, 0.456, 0.406])
-    # std = np.array([0.229, 0.224, 0.225])
-    # inp = std * inp + mean
-    # inp = np.clip(inp, 0, 1)
     ax.imshow(inp)
     if title is not None:
         ax.set_title(title)
