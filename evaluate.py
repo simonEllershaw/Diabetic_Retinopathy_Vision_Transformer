3adce3f17a733cbd4dbcad8a21746089a84267e1
Simon Ellershaw <simje@live.co.uk>
2021-07-05 19:20:12 +0100

    added thresholding based on auc

--- evaluate.py
+++ evaluate.py
@@ -37,7 +37,7 @@ def get_predictions(model, dataloader, num_samples, batch_size, device):
     return prob_log, pred_log
 
 def init_metrics_log_dict():
-    keys = ["accuracy", "precision_score", "recall_score", "f1", "Pre/Rec AUC", "ROC AUC", "conf_matrix"]
+    keys = ["accuracy", "threshold", "precision_score", "recall_score", "f1", "Pre/Rec AUC", "ROC AUC", "conf_matrix"]
     metrics_log = {key: [] for key in keys}
     return metrics_log
 
@@ -51,14 +51,19 @@ def calc_metrics(labels, pred_log, metrics_log):
 
 def plot_precision_recall_curve(labels, prob_log, ax, model_name):
     precision, recall, thresholds = sklearn.metrics.precision_recall_curve(labels, prob_log)
+    fscore = (2 * precision * recall) / (precision + recall)
+    ix = np.argmax(fscore)
+    threshold = thresholds[ix]
     auc = sklearn.metrics.auc(recall, precision)
     pr_display = sklearn.metrics.PrecisionRecallDisplay(precision=precision, recall=recall).plot(ax, label=model_name)
+    ax.scatter(recall[ix], precision[ix], marker='o', color='black', label=f'Best {model_name}')
+    
     no_skill = len(labels[labels==1]) / len(labels)
     if "No Skill" not in ax.get_legend_handles_labels()[1]:
         ax.plot([0, 1], [no_skill, no_skill], linestyle='--', label='No Skill')
     ax.set_xlim(0,1)
     ax.set_ylim(0,1.1)
-    return auc
+    return auc, threshold
 
 def plot_ROC_curve(labels, prob_log, ax, model_name):
     fpr, tpr, _ = sklearn.metrics.roc_curve(labels, prob_log)
@@ -71,18 +76,23 @@ def plot_ROC_curve(labels, prob_log, ax, model_name):
     return auc
 
 def evaluate_models(models, device, dataloader, labels, batch_size, eval_directory, phase, model_names):
-    directory = os.path.join(eval_directory, time.strftime('%m_%d_%H_%M_%S'))
-    os.mkdir(directory)
     fig, axs = plt.subplots(1, 2)
     metrics_log = init_metrics_log_dict()
     for model, model_name in zip(models, model_names):
         model.eval()
         prob_log, pred_log = get_predictions(model, dataloader, len(labels), batch_size, device)
-        
-        metrics_log = calc_metrics(labels, pred_log, metrics_log)
-        metrics_log["Pre/Rec AUC"].append(plot_precision_recall_curve(labels, prob_log, axs[0], model_name))
+
+        auc, threshold = plot_precision_recall_curve(labels, prob_log, axs[0], model_name)
+        metrics_log["Pre/Rec AUC"].append(auc)
+        metrics_log["threshold"].append(threshold)
         metrics_log["ROC AUC"].append(plot_ROC_curve(labels, prob_log, axs[1], model_name))
-    
+        pred_log[prob_log>=threshold] = 1
+        pred_log[prob_log<threshold] = 0
+        metrics_log = calc_metrics(labels, pred_log, metrics_log)
+        
+    directory = os.path.join(eval_directory, time.strftime('%m_%d_%H_%M_%S'))
+    os.mkdir(directory)
+
     axs[0].legend()
     axs[1].legend()
     fig.set_size_inches(18.5, 10.5)
@@ -95,16 +105,16 @@ if __name__ == "__main__":
     # Load datasets split into train, val and test
     print(sys.argv)
     data_directory = sys.argv[1] if len(sys.argv) > 1 else "diabetic-retinopathy-detection"
-    model_directorys = sys.argv[2] if len(sys.argv) > 2 else ["runs\\06_30_Grid_Search\\resnetv2_50x1_bitm_in21k\\0.01", "runs\\06_30_Grid_Search\\vit_small_patch16_224_in21k\\0.01"]
+    model_directories = sys.argv[2] if len(sys.argv) > 2 else ["runs\\06_30_Grid_Search\\resnetv2_50x1_bitm_in21k\\0.01", "runs\\06_30_Grid_Search\\vit_small_patch16_224_in21k\\0.01"]
     model_names = sys.argv[3] if len(sys.argv) > 3 else ["resnetv2_50x1_bitm_in21k", "vit_small_patch16_224_in21k"]
     phase = sys.argv[4] if len(sys.argv) > 4 else "test"
     eval_directory = "eval_data"
 
     dataset_proportions = np.array([0.6, 0.2, 0.2])
-    full_dataset = EyePACS_Dataset(data_directory, random_state=13)
+    full_dataset = EyePACS_Dataset(data_directory, random_state=13, img_size=224)
     class_names = full_dataset.class_names
     datasets = full_dataset.create_train_val_test_datasets(dataset_proportions, ["train", "val", "test"])
-    batch_size = 100
+    batch_size = 64
     dataloader = torch.utils.data.DataLoader(datasets[phase], batch_size=batch_size, shuffle=False, num_workers=4) 
     labels = datasets[phase].get_labels()
     
