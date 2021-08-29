88c34b85ab0ec34bb606fc8b6225f8de3d0945c5
Simon Ellershaw <simje@live.co.uk>
2021-08-29 18:10:41 +0100

    Refactored so works without too much user input

--- preprocess.py
+++ preprocess.py
@@ -1,85 +1,41 @@
-from PIL import ImageFilter, Image
+import os
 import numpy as np
-import math
-import cv2  
+from cv2 import cv2
 
-import matplotlib.pyplot as plt
-import time
-import sys
-import os 
-
-def preprocess_all_images(img_dir, img_dir_preprocessed, image_format, seg_dir=None, seg_dir_preprocessed=None, seg_format=None):
+def preprocess_all_images(img_dir, image_format, img_dir_preprocessed, preprocessed_size, store_crop_boxes=False):
+    # Create directories
     if not os.path.exists(img_dir_preprocessed):
         os.makedirs(img_dir_preprocessed)
+    crop_boxes={}
 
+    # Preprocess each image
     for fname in os.listdir(img_dir):
         if fname.endswith(image_format):
+            # Read in, calc crop box, preprocess and save to file
             img = cv2.imread(os.path.join(img_dir, fname))
-            img, x_min, y_min, radius_inital = GrahamPreprocessing(img)
+            x_min, y_min, radius_inital = calc_cropbox_dim(img)
+            if radius_inital < 10:
+                raise ValueError("Image radius could not be found")
+            img = preprocess_img(img, x_min, y_min, radius_inital, preprocessed_size)
             cv2.imwrite(os.path.join(img_dir_preprocessed, fname), img)
-            if seg_dir is not None:
-                if not os.path.exists(seg_dir_preprocessed):
-                    os.makedirs(seg_dir_preprocessed)
-                fname = fname[:-3] + seg_format
-                seg = cv2.imread(os.path.join(seg_dir, fname))
-                seg, _, _, _ = GrahamPreprocessing(seg, x_min, y_min, radius_inital)
-                cv2.imwrite(os.path.join(seg_dir_preprocessed, fname), seg)
+            # If seg maps use crop box defined by RGB image
+            if store_crop_boxes:
+                # Remove file type from fname
+                fname = fname[:-len(image_format)]
+                crop_boxes[fname] = {"x_min": x_min, "y_min": y_min, "radius_inital": radius_inital}
+    return crop_boxes    
 
-
-def GrahamPreprocessing(img, x_min_no_pad=None, y_min_no_pad=None, radius_inital=None):
-    if x_min_no_pad is None:
-        x_min_no_pad, y_min_no_pad, radius_inital = calc_cropbox_dim(img)
-
-    if radius_inital < 10:
-        raise ValueError("Image radius could not be found")
-
-    img, x_min, y_min = pad_image(img, x_min_no_pad, y_min_no_pad)
-    img = img[y_min:y_min+radius_inital*2, x_min:x_min+radius_inital*2]
-    radius_scaled = 500       
-    img = rescale_image(img, radius_inital, radius_scaled)
-    # img = subtract_average_local_colour(img)
-    # img = threshold_boundary(img, round(radius_scaled*0.9))
-    img = cv2.resize(img, [448,448])
-    return img, x_min_no_pad, y_min_no_pad, radius_inital
-
-def estimate_radius(image):
-    # Take central row and sum accross channels
-    x = image[image.shape[0]//2].sum(1)
-    # ROI defined as non zero pixels
-    radius = (x>x.mean()/10).sum()/2
-    return radius
-
-def rescale_image(image, radius_intial, radius_scaled):
-    width, height, _ = image.shape
-    # Resize so radius of eye is = radius_scaled
-    s = radius_scaled/radius_intial
-    return cv2.resize(image, (0,0), fx=s, fy=s)
-
-def subtract_average_local_colour(image):
-    average_local_colour = cv2.GaussianBlur(image, (0,0), 10)
-    image = cv2.addWeighted(image, 4, average_local_colour, -4, 128)
-    return image
-
-def threshold_boundary(img, radius_boundary):
-    boundary = np.zeros(img.shape)
-    cv2.circle(boundary, (img.shape[1]//2, img.shape[0]//2), radius_boundary, (1,1,1), -1)
-    boundary = boundary.astype("uint8")
-    img = cv2.multiply(img, boundary) + 128*(1-boundary)
+def preprocess_img(img, x_min, y_min, radius_inital, preprocessed_size):
+    # Pad so crop box in RGB image
+    img, x_min_pad, y_min_pad = pad_image(img, x_min, y_min)
+    # Crop
+    img = img[y_min_pad:y_min_pad+radius_inital*2, x_min_pad:x_min_pad+radius_inital*2]
+    # Resize
+    img = cv2.resize(img, [preprocessed_size, preprocessed_size])
     return img
 
-def crop_image(image, radius_boundary):
-    # Crop to square the size of scale
-    centre_h = image.shape[0]//2
-    centre_w = image.shape[1]//2
-    if centre_h < radius_boundary:
-        image, centre_h = pad_height(image, 2*radius_boundary)
-    y_min = centre_h - radius_boundary
-    y_max = centre_h + radius_boundary
-    x_min = centre_w - radius_boundary
-    x_max = centre_w + radius_boundary
-    return image[y_min:y_max, x_min:x_max]
-
 def pad_image(image, x_min, y_min):
+    # If x_min or y_min pad so they are at 0
     h_padding, v_padding = (0,0)
     if x_min < 0:
         h_padding = -x_min
@@ -114,32 +70,74 @@ def calc_cropbox_dim(img):
     top_left_coord = ((center - radius)).astype(int)
     return top_left_coord[0], top_left_coord[1], round(radius)
 
-if __name__ == "__main__":    
-    # for sub_dir in os.listdir(img_dir):
-    #     print(sub_dir)
-    #     if "Base" in sub_dir:
-    #         img_dir_preprocessed = os.path.join(img_dir, "preprocessed_448", sub_dir)
-    #         preprocess_all_images(os.path.join(img_dir, sub_dir), img_dir_preprocessed, ".tif")
+def preprocess_seg_map(fname, seg_sub_dir, seg_dir_preprocessed, x_min, y_min, radius_inital, preprocessed_size):
+    seg_map = create_seg_map(fname, seg_sub_dir)
+    seg_map_preprocessed = preprocess_img(seg_map, x_min, y_min, radius_inital, preprocessed_size)
+    cv2.imwrite(os.path.join(seg_dir_preprocessed, fname + seg_image_format), seg_map_preprocessed)
 
-    data_directory = r'data\idrid'
-    img_dir = os.path.join(data_directory, "Images")
-    seg_dir = os.path.join(data_directory, "Segmentation", "5. Mask")
-    img_preprocessed_dir = os.path.join(data_directory, "Images_Preprocessed")
-    seg_preprocessed_dir = os.path.join(data_directory, "Segmentation_Preprocessed")
-
-    preprocess_all_images(img_dir, img_preprocessed_dir, "jpg", seg_dir=seg_dir, seg_dir_preprocessed=seg_preprocessed_dir, seg_format="tif")
-
-    # fig, axes = plt.subplots(1,3, figsize=(12,6))
-    # start_time = time.time()
-    # image = cv2.imread(r"C:\\Users\\rmhisje\Documents\\medical_ViT\\data\\eyePACs\\train\\train\\10_left.jpeg")
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # axes[0].imshow(image)
-    # out = GrahamPreprocessing(image)
-    # # print(image)
-    # # print(time.time()-start_time)
-    # axes[2].imshow(out[0])
-    # titles = ["Input Image", "Threshold Mask", "Preprocessed Image"]
-    # for ax, title in zip(axes, titles):
-    #     ax.set_xlabel(title)
+def create_seg_map(fname, seg_dir):
+    # Define lesion directories
+    dir_microaneurysms = os.path.join(seg_dir, "1. Microaneurysms")
+    dir_haemorrhages = os.path.join(seg_dir, "2. Haemorrhages")
+    dir_hard_exudates = os.path.join(seg_dir, "3. Hard Exudates")
+    dir_soft_exudates = os.path.join(seg_dir, "4. Soft Exudates")
     
-    # plt.show()
+    # Add each lesion to channel of seg map (BGR as using cv2)
+    # Red
+    seg_map = cv2.imread(os.path.join(dir_microaneurysms, f"{fname}_MA.tif"))
+    # Green
+    haemorrhages_fname = os.path.join(dir_haemorrhages, f"{fname}_HE.tif")
+    seg_map = add_lesion_to_seg_map(seg_map, haemorrhages_fname, [1])
+    # Blue
+    hard_exudates_fname = os.path.join(dir_hard_exudates, f"{fname}_EX.tif")
+    seg_map = add_lesion_to_seg_map(seg_map, hard_exudates_fname, [0])
+    # Pink
+    soft_exudates_fname = os.path.join(dir_soft_exudates, f"{fname}_SE.tif")
+    seg_map = add_lesion_to_seg_map(seg_map, soft_exudates_fname, [0,2])
+    # Clip required due to pink channel
+    seg_map = np.clip(seg_map, 0, 255)
+    return seg_map
+
+def add_lesion_to_seg_map(seg_map, lesion_fname, channels):
+    # All lesions held in red channel add to segmap channel of choice
+    if os.path.isfile(lesion_fname):
+        for channel in channels:
+            seg_map[:,:,channel] += cv2.imread(lesion_fname)[:,:,2]
+    return seg_map
+
+if __name__ == "__main__":
+    preprocessed_size = 448
+
+    # # EyePACs Preprocessing
+    data_directory = os.path.join("data", "eyePACs")
+    img_dir = os.path.join(data_directory, "train", "train")
+    image_format = ".jpeg"
+    img_dir_preprocessed = os.path.join(data_directory, "preprocessed_images")
+    preprocess_all_images(img_dir, image_format, img_dir_preprocessed, preprocessed_size)
+
+    # Messidor Preprocessing
+    data_directory = os.path.join("data", "messidor")
+    image_format = ".tif"
+    preprocessed_size = 448
+    for sub_dir in os.listdir(data_directory):
+        img_dir = os.path.join(data_directory, sub_dir)
+        if os.path.isdir(img_dir) and "Base" in sub_dir:
+            img_dir_preprocessed = os.path.join(data_directory, "preprocessed_images", sub_dir)
+            preprocess_all_images(img_dir, image_format, img_dir_preprocessed, preprocessed_size)
+
+    # IDRiD Processing- Includes creating and preprocessing seg_maps as well
+    data_directory = os.path.join("data", "idrid")
+    img_dir = os.path.join(data_directory, "A. Segmentation", "A. Segmentation", "1. Original Images")
+    img_dir_preprocessed = os.path.join(data_directory, "preprocessed_images")
+    image_format = ".jpg"
+    seg_dir = os.path.join(data_directory, "A. Segmentation", "A. Segmentation", "2. All Segmentation Groundtruths")
+    seg_dir_preprocessed = os.path.join(data_directory, "preprocessed_seg")
+    seg_image_format = ".tif"
+
+    for sub_dir in os.listdir(img_dir):
+        img_sub_dir = os.path.join(img_dir, sub_dir)
+        seg_sub_dir = os.path.join(seg_dir, sub_dir)
+        crop_boxes = preprocess_all_images(img_sub_dir, image_format, img_dir_preprocessed, preprocessed_size, store_crop_boxes=True)
+        for fname, crop_boxes_dim in crop_boxes.items():
+            x_min, y_min, radius_inital = crop_boxes_dim.values()
+            preprocess_seg_map(fname, seg_sub_dir, seg_dir_preprocessed, x_min, y_min, radius_inital, preprocessed_size)
