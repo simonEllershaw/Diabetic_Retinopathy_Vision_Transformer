f531bcf95251794afc4ef51498935de6f0ddb1a8
Simon Ellershaw <simje@live.co.uk>
2021-08-30 12:27:25 +0100

    IDRid uses super class get item method

--- datasets/IDRiD.py
+++ datasets/IDRiD.py
@@ -19,27 +19,28 @@ class IDRiD_Dataset(Abstract_DR_Dataset):
 
     def load_labels(self, max_length, **kwargs):
         length = len(os.listdir(self.img_dir_preprocessed)) if max_length is None else max_length
-        labels_df = pd.DataFrame({"index": np.arange(length, dtype=int), 'label': np.ones(length, dtype=int)})
+        fnames = [self.get_fname(idx) for idx in np.arange(length, dtype=int)]
+        image_names = [fname + ".jpg" for fname in fnames]
+        seg_names = [fname + ".tif" for fname in fnames]
         # All examples are referable DR cases (as have annoated lesions!)
+        levels = np.ones(length, dtype=int)
+        labels_df = pd.DataFrame({"image_name": image_names, "seg_name": seg_names, "level": levels})
+        print(labels_df.head())
         return labels_df
 
     def __getitem__(self, idx):
-        # Extract sample's metadata
-        sample = self.labels_df.loc[idx]
-        idx = sample["index"]
-        label = sample["label"]
-        fname = self.get_fname(idx)
-        img_path = os.path.join(self.img_dir_preprocessed, fname + ".jpg")
-        img = Image.open(img_path)
-        img = self.get_augmentations()(img)
+        # Overwrites super class def as seg maps loaded as well
+        img, label, image_name = super().__getitem__(idx)
 
-        seg_path = os.path.join(self.seg_dir_preprocessed, fname + ".tif")
+        # Get seg maps
+        metadata = self.labels_df.loc[idx]
+        seg_path = os.path.join(self.seg_dir_preprocessed, metadata.seg_name)
         seg = Image.open(seg_path)
         seg = torchvision.transforms.Resize(self.img_size)(seg)
         seg = np.array(seg, dtype="float")
         seg[seg>0] = 1.0
         seg_cuml = self.generate_seg_cum(seg)
-        return img, seg, seg_cuml, label, fname
+        return img, seg, seg_cuml, label, image_name
 
     def get_fname(self, idx):
         # Indexed from 01
@@ -51,6 +52,7 @@ class IDRiD_Dataset(Abstract_DR_Dataset):
         return seg_cum
 
     def visualise_sample(self, idx):
+        # Overides superclass method to show seg maps as well
         fig, axes = plt.subplots(1, 3)
         titles = ["Input Image", "Annotations", "Patched Annotations"]
         image, seg, seg_cum, label, fname = self[idx]
